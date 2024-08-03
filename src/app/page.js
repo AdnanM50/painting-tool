4c9a2c6962ed2564419e3e93da82325e2b7a253c
Adnan <87126125+Adnan42@users.noreply.github.com>
2024-08-04 00:35:15 +0600

    still feacing error and can not solve it

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import Board from "@/components/board";
 import Menu from "@/components/menu";
 import Toolbox from "@/components/toolbox";
 
@@ -7,6 +8,7 @@ export default function Home() {
   <>
   <Menu />
   <Toolbox />
+  <Board />
   </>
   );
 }
